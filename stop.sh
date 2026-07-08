811e46651d9e1a7250af49a13616dcb2221dc2c9
garlapatisai <saikrishnasai50931@gmail.com>
2026-07-08 23:58:25 +0530

    Rename Streamlit main file

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Stopping all AI Time Management services...'
-kill -9 11526 11527 11528 2>/dev/null
+kill -9 12972 12973 12974 2>/dev/null
 kill -9 $(lsof -t -i:8000) $(lsof -t -i:3000) 2>/dev/null
 pkill -f 'python agent.py' 2>/dev/null
 echo 'All services stopped!'
